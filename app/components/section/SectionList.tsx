53cc43a3308023d9a89d586d98a7b99600ba9e39
nukko <shouta.tsuboi@gmail.com>
2024-11-28 09:53:30 +0900

    並び替え処理の実装

--- app/components/section/SectionList.tsx
+++ app/components/section/SectionList.tsx
@@ -4,7 +4,12 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import type { AppDispatch, RootState } from '@/app/store/store'
 import { fetchSections } from '@/app/features/section/sectionSlice'
-import { GridList, GridListItem } from 'react-aria-components'
+import {
+	DropIndicator,
+	GridList,
+	GridListItem,
+	useDragAndDrop,
+} from 'react-aria-components'
 import SectionItem from './SectionItem'
 
 interface SectionListProps {
@@ -31,6 +36,29 @@ const SectionList = ({ spaceId }: SectionListProps) => {
 	if (!spaceId) return <div>スペースが選択されていません</div>
 	if (loading) return <div>読み込み中...</div>
 	if (error) return <div>エラー: {error}</div>
+
+	const { dragAndDropHooks } = useDragAndDrop({
+		getItems: (key) =>
+			[...key].map((key) => ({
+				'text/plain': key.toString(),
+				'application/json': JSON.stringify(
+					sections.find((section) => section.id === key),
+				),
+			})),
+		onReorder(e) {
+			// 並び替え処理
+		},
+		renderDropIndicator(target) {
+			return (
+				<DropIndicator
+					target={target}
+					className={({ isDropTarget }) =>
+						`h-1 bg-blue-500/50 rouded transition-all ${isDropTarget ? 'bg-blue-500' : ''}`
+					}
+				/>
+			)
+		},
+	})
 
 	return (
 		<GridList className="flex flex-col w-full gap-2">
