c4acb98fefbf6903c2992e1bbb4afba0af5f0a5e
nukko <shouta.tsuboi@gmail.com>
2024-12-11 06:45:45 +0900

    パフォーマンスの改善

--- app/features/performance/performance.ts
+++ app/features/performance/performance.ts
@@ -1,8 +1,17 @@
 // performance.ts
 export const measurePerformance = (actionName: string) => {
 	const start = performance.now()
+	const startEntries = performance.getEntriesByType('resource')
+
 	return () => {
 		const end = performance.now()
-		console.log(`${actionName} took ${end - start}ms`)
+		const endEntries = performance.getEntriesByType('resource')
+		const newEntries = endEntries.slice(startEntries.length)
+
+		console.log(`${actionName}:
+			Total Time: ${end - start}ms
+			Network Requests: ${newEntries.length}
+			Resources: ${newEntries.map((e) => e.name).join(', ')}
+		`)
 	}
 }
